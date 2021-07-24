c96e3ce6c6b81f3a068c929848bf92663d498f9d
ticahmfock <294962719@qq.com>
2021-07-24 16:27:29 +0800

    新增认证中心模块

---
+++ oauth-center/src/main/java/com/tk/Oauth.java
@@ -0,0 +1,13 @@
+package com.tk;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: TK
+ * @Time: 2021/7/24 16:19
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class Oauth {
+}
